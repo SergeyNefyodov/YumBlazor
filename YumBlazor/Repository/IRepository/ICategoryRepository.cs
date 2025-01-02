32674fa32f6809aff99f9bb60654086baf721af1
SergeyNefyodov <nefyodov.sergei@gmail.com>
2025-01-02 20:45:03 +0100

    Create ICategoryRepository

---
+++ YumBlazor/Repository/IRepository/ICategoryRepository.cs
@@ -0,0 +1,12 @@
+using YumBlazor.Data;
+
+namespace YumBlazor.Repository.IRepository;
+
+public interface ICategoryRepository
+{
+    Category Create();
+    Category Update(Category category);
+    bool Delete(int id);
+    Category Get(int id);
+    IEnumerable<Category> GetAll();
+}
