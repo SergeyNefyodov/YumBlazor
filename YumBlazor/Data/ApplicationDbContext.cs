2b091c8766870a0a35f89bdbdf7c3f15442340d6
SergeyNefyodov <nefyodov.sergei@gmail.com>
2025-01-02 19:41:20 +0100

    Setup project

--- YumBlazor/Data/ApplicationDbContext.cs
+++ YumBlazor/Data/ApplicationDbContext.cs
@@ -3,6 +3,7 @@ using Microsoft.EntityFrameworkCore;
 
 namespace YumBlazor.Data;
 
-public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options)
+public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+    : IdentityDbContext<ApplicationUser>(options)
 {
 }
--- YumBlazor/Data/ApplicationUser.cs
+++ YumBlazor/Data/ApplicationUser.cs
@@ -6,4 +6,3 @@ namespace YumBlazor.Data;
 public class ApplicationUser : IdentityUser
 {
 }
-
